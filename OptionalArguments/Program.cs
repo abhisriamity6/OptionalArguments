ec635b8b03e21f5767b9e573cf351f19395185c7
Administrator <abhi_sri_amity6@yahoo.com>
2017-06-21 23:33:14 +0530

    completed

--- OptionalArguments/Program.cs
+++ OptionalArguments/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace OptionalArguments
 {
+
+    class OptionalArgsDemo
+    {
+        public void OptArgsMeth(int a , int b =10, int c = 100)
+        {
+
+            Console.WriteLine("Value of 1st argument: {0} ,Value of 2nd argument: {1} ,Value of 3rd argument: {2}", a, b, c);
+        }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            OptionalArgsDemo obj = new OptionalArgsDemo();
+            obj.OptArgsMeth(1);
+            obj.OptArgsMeth(1,2);
+            obj.OptArgsMeth(1,2,3);
+            Console.Read();
         }
     }
 }
